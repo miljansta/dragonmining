784cbbd3f894795aa48a2af8f0453130373815e8
Acke <acidfelix1986@gmail.com>
2018-02-11 19:40:51 +0100

    home

--- view/scripts/js/script.js
+++ view/scripts/js/script.js
@@ -32,10 +32,10 @@ function carousel() {
     var i;
     var x = document.getElementsByClassName("mySlides");
     for (i = 0; i < x.length; i++) {
-       x[i].style.display = "none";  
+       x[i].style.opacity = 0;  
     }
     myIndex++;
     if (myIndex > x.length) {myIndex = 1}    
-    x[myIndex-1].style.display = "block";  
+    x[myIndex-1].style.opacity = 1;  
     setTimeout(carousel, 4000);
 }
